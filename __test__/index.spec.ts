e5e73a2e3757beb305d832e4942434a7d2ccbc04
Your Name <you@example.com>
2024-06-24 11:00:52 +0800

    #FIX: loss key of button

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -1,11 +1,5 @@
 import test from 'ava'
 
-import { startListener } from '../index'
-
 test('sync function from native code', (t) => {
   t.pass()
-  startListener((data) => {
-    console.log(data)
-  })
-  console.log('startListener')
 })
--- src/lib.rs
+++ src/lib.rs
@@ -1,68 +1,70 @@
 // src/lib.rs
 #![deny(clippy::all)]
 
-use napi_derive::napi;
+use napi::threadsafe_function::{ErrorStrategy, ThreadsafeFunction, ThreadsafeFunctionCallMode};
 use napi::{JsFunction, Result};
-use serde_json::{self, json};
+use napi_derive::napi;
 use serde::Serialize;
+use serde_json;
 use std::thread::spawn;
 use windows::Win32::Foundation::{HINSTANCE, LPARAM, LRESULT, WPARAM};
 use windows::Win32::UI::WindowsAndMessaging::{
-    CallNextHookEx, DispatchMessageW, GetMessageW, SetWindowsHookExW, TranslateMessage, MSG,
-    WH_MOUSE_LL, WM_LBUTTONDOWN, WM_LBUTTONUP, WM_MOUSEMOVE, WM_RBUTTONDOWN, WM_RBUTTONUP,
-    MSLLHOOKSTRUCT,
+  CallNextHookEx, DispatchMessageW, GetMessageW, SetWindowsHookExW, TranslateMessage, MSG,
+  MSLLHOOKSTRUCT, WH_MOUSE_LL, WM_LBUTTONDOWN, WM_LBUTTONUP, WM_MOUSEMOVE, WM_RBUTTONDOWN,
+  WM_RBUTTONUP,
 };
-use napi::threadsafe_function::{ErrorStrategy, ThreadsafeFunction, ThreadsafeFunctionCallMode};
 
 #[derive(Serialize)]
 struct MouseEvent {
-    event_type: String,
-    x: i32,
-    y: i32,
+  event_type: String,
+  x: i32,
+  y: i32,
+  key: Option<String>,
 }
 
-unsafe extern "system" fn mouse_proc(
-    code: i32,
-    wparam: WPARAM,
-    lparam: LPARAM,
-) -> LRESULT {
-    if code >= 0 {
-        let mouse_info = &*(lparam.0 as *const MSLLHOOKSTRUCT);
-        let event = match wparam.0 as u32 {
-            WM_MOUSEMOVE => MouseEvent {
-                event_type: "MouseMove".to_string(),
-                x: mouse_info.pt.x,
-                y: mouse_info.pt.y,
-            },
-            WM_LBUTTONDOWN => MouseEvent {
-                event_type: "ButtonPress".to_string(),
-                x: mouse_info.pt.x,
-                y: mouse_info.pt.y,
-            },
-            WM_LBUTTONUP => MouseEvent {
-                event_type: "ButtonRelease".to_string(),
-                x: mouse_info.pt.x,
-                y: mouse_info.pt.y,
-            },
-            WM_RBUTTONDOWN => MouseEvent {
-                event_type: "ButtonPress".to_string(),
-                x: mouse_info.pt.x,
-                y: mouse_info.pt.y,
-            },
-            WM_RBUTTONUP => MouseEvent {
-                event_type: "ButtonRelease".to_string(),
-                x: mouse_info.pt.x,
-                y: mouse_info.pt.y,
-            },
-            _ => return CallNextHookEx(None, code, wparam, lparam),
-        };
+unsafe extern "system" fn mouse_proc(code: i32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
+  if code >= 0 {
+    let mouse_info = &*(lparam.0 as *const MSLLHOOKSTRUCT);
+    let event = match wparam.0 as u32 {
+      WM_MOUSEMOVE => MouseEvent {
+        event_type: "MouseMove".to_string(),
+        x: mouse_info.pt.x,
+        y: mouse_info.pt.y,
+        key: None,
+      },
+      WM_LBUTTONDOWN => MouseEvent {
+        event_type: "ButtonPress".to_string(),
+        x: mouse_info.pt.x,
+        y: mouse_info.pt.y,
+        key: Some("Left".to_string()),
+      },
+      WM_LBUTTONUP => MouseEvent {
+        event_type: "ButtonRelease".to_string(),
+        x: mouse_info.pt.x,
+        y: mouse_info.pt.y,
+        key: Some("Left".to_string()),
+      },
+      WM_RBUTTONDOWN => MouseEvent {
+        event_type: "ButtonPress".to_string(),
+        x: mouse_info.pt.x,
+        y: mouse_info.pt.y,
+        key: Some("Right".to_string()),
+      },
+      WM_RBUTTONUP => MouseEvent {
+        event_type: "ButtonRelease".to_string(),
+        x: mouse_info.pt.x,
+        y: mouse_info.pt.y,
+        key: Some("Right".to_string()),
+      },
+      _ => return CallNextHookEx(None, code, wparam, lparam),
+    };
 
-        if let Some(jsfn) = &*JSFN.lock().unwrap() {
-            let event_json = serde_json::to_string(&event).unwrap();
-            jsfn.call(event_json, ThreadsafeFunctionCallMode::NonBlocking);
-        }
+    if let Some(jsfn) = &*JSFN.lock().unwrap() {
+      let event_json = serde_json::to_string(&event).unwrap();
+      jsfn.call(event_json, ThreadsafeFunctionCallMode::NonBlocking);
     }
-    CallNextHookEx(None, code, wparam, lparam)
+  }
+  CallNextHookEx(None, code, wparam, lparam)
 }
 
 lazy_static::lazy_static! {
@@ -71,26 +73,19 @@ lazy_static::lazy_static! {
 
 #[napi(ts_args_type = "callback: (event: string) => void")]
 pub fn start_listener(callback: JsFunction) -> Result<()> {
-    let jsfn: ThreadsafeFunction<String, ErrorStrategy::Fatal> =
-        callback.create_threadsafe_function(0, |ctx| Ok(vec![ctx.value]))?;
+  let jsfn: ThreadsafeFunction<String, ErrorStrategy::Fatal> =
+    callback.create_threadsafe_function(0, |ctx| Ok(vec![ctx.value]))?;
 
-    *JSFN.lock().unwrap() = Some(jsfn);
+  *JSFN.lock().unwrap() = Some(jsfn);
 
-    spawn(move || {
-        unsafe {
-            let hook = SetWindowsHookExW(
-                WH_MOUSE_LL,
-                Some(mouse_proc),
-                HINSTANCE(0),
-                0,
-            );
-            let mut msg: MSG = MSG::default();
-            while GetMessageW(&mut msg, None, 0, 0).as_bool() {
-                TranslateMessage(&msg);
-                DispatchMessageW(&msg);
-            }
-        }
-    });
+  spawn(move || unsafe {
+    let hook = SetWindowsHookExW(WH_MOUSE_LL, Some(mouse_proc), HINSTANCE(0), 0);
+    let mut msg: MSG = MSG::default();
+    while GetMessageW(&mut msg, None, 0, 0).as_bool() {
+      TranslateMessage(&msg);
+      DispatchMessageW(&msg);
+    }
+  });
 
-    Ok(())
+  Ok(())
 }
